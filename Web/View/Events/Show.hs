2c982a594a373d22008a4f8301464037aca825a2
Amitai Burstein <amitai@gizra.com>
2021-11-05 11:35:18 +0200

    Copy paste

--- Web/View/Events/Show.hs
+++ Web/View/Events/Show.hs
@@ -15,10 +15,26 @@ instance View ShowView where
             <svg xmlns="http://www.w3.org/2000/svg" class="mt-1 h-5 w-5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <p>
-                This area is meant for the event organizer to view the reservations.
-                But we are also going to use it for the attendees to register to the event.
-            </p>
+            <div class="flex flex-col space-y-6">
+                <p>
+                    This area is meant for the event organizer to view the reservations.
+                    But we are also going to use it for the attendees to register to the event.
+                </p>
+                <p>
+                    You can test how twenty concurrent requests would look using <code>parallel</code>
+
+                    <div class="copy-paste flex flex-row items-center">
+                        <input class="rounded-l border-yellow-500 text-gray-500 w-4/5" type="text" value={copyPasteCode} readonly />
+                        <button class="rounded-r border-r border-t border-b border-yellow-500 hover:bg-yellow-200">
+                            <!-- https://heroicons.com : duplicate -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                            </svg>
+                        </button>
+
+                    </div>
+                </p>
+            </div>
         </div>
 
         <h1 class="text-3xl">Reservations for {get #title event} Event</h1>
@@ -51,6 +67,10 @@ instance View ShowView where
                 reservations
                     |> filter (\reservation -> get #status reservation == Accepted)
                     |> length
+
+            baseUrl = getConfig |> get #baseUrl
+
+            copyPasteCode = "time seq 20 | parallel -n0 \"curl '" ++ baseUrl ++ "/CreateReservation' -H 'content-type: application/x-www-form-urlencoded' --data-raw 'eventId=eca8da2a-8748-483f-97ba-4dcbad6cf5e5&personIdentifier=1234' --compressed\"" :: Text
 
 
 renderReservations event reservations =
--- static/app.js
+++ static/app.js
@@ -1,3 +1,12 @@
 $(document).on('ready turbolinks:load', function () {
-    // This is called on the first page load *and* also when the page is changed by turbolinks
+
+    $('.copy-paste button').click(function () {
+        const text = $(this).parent('.copy-paste').find('input').val();
+
+        navigator.clipboard.writeText(text).then(function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    });
 });
