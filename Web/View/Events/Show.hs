ff8d79d3df01e81f277df6f4d2fef3d78c51d343
Amitai Burstein <amitai@gizra.com>
2021-11-05 18:38:00 +0200

    Helper for cards

--- Web/View/Events/Show.hs
+++ Web/View/Events/Show.hs
@@ -29,6 +29,7 @@ instance View ShowView where
                         <button class="rounded-r border-r border-t border-b border-yellow-500 hover:bg-yellow-200">
                             <!-- https://heroicons.com : duplicate -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <title>Copy code</title>
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
                             </svg>
                         </button>
