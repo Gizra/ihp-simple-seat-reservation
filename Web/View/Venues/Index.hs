0dd207b1bca760da461b806ebb0b4f7daf9be9f1
Amitai Burstein <amitai@gizra.com>
2021-11-07 18:51:27 +0200

    More theme

--- Web/View/Venues/Index.hs
+++ Web/View/Venues/Index.hs
@@ -9,8 +9,8 @@ instance View IndexView where
 
         <h1>Index<a href={pathTo NewVenueAction} class="btn btn-primary">+ New</a></h1>
         <div class="table-responsive">
-            <table class="table">
-                <thead>
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-100">
                     <tr>
                         <th class={thClasses}>Venue</th>
                         <th class={thClasses}>Ops</th>
@@ -31,10 +31,13 @@ instance View IndexView where
 renderVenue :: Venue -> Html
 renderVenue venue = [hsx|
     <tr>
-        <td><a class="text-blue-500 hover:text-blue-600 hover:underline" href={ShowVenueAction (get #id venue)}>{get #title venue}</a></td>
-        <td class="flex flex-row space-x-2">
+        <td class={tdClasses}><a class="text-blue-500 hover:text-blue-600 hover:underline" href={ShowVenueAction (get #id venue)}>{get #title venue}</a></td>
+        <td class={tdClassesWithSeparator}>
             <a href={EditVenueAction (get #id venue)} class="text-muted">Edit</a>
             <a href={DeleteVenueAction (get #id venue)} class="js-delete text-muted">Delete</a>
         </td>
     </tr>
 |]
+    where
+        tdClasses = "px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900" :: Text
+        tdClassesWithSeparator = classes ["flex flex-row space-x-2", (tdClasses, True)]
