cc8d874f4624792eeaddf315e4f301799f0254cf
Amitai Burstein <amitai@gizra.com>
2023-04-09 00:53:42 +0300

    Skip on empty HTML

--- Web/Element/ElementWrap.hs
+++ Web/Element/ElementWrap.hs
@@ -2,7 +2,10 @@ module Web.Element.ElementWrap where
 
 import Web.View.Prelude
 import qualified Text.Blaze.Html5 as Html5
+import Text.Blaze.Internal
 
 wrapContainerVerticalSpacingTiny :: Html -> Html
 wrapContainerVerticalSpacingTiny element =
-    [hsx|<div class="flex flex-col gap-y-2">{element}</div>|]
+    case element of
+        Empty _ -> [hsx||]
+        _ -> [hsx|<div class="flex flex-col gap-y-2">{element}</div>|]
