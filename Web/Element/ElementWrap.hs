1bf3ce37211d3d4924bb79c887f9f7fd7eb01e15
Amitai Burstein <amitai@gizra.com>
2023-04-09 15:31:00 +0300

    Abstract

--- Web/Element/ElementWrap.hs
+++ Web/Element/ElementWrap.hs
@@ -4,20 +4,15 @@ import Web.View.Prelude
 import qualified Text.Blaze.Html5 as Html5
 import Text.Blaze.Internal
 
-wrapContainerVerticalSpacing :: [Html5.Html] -> Html5.Html
+wrapContainerVerticalSpacing :: [Html5.Html] -> Html
 wrapContainerVerticalSpacing elements =
     let
-        filterElements =
-            filter (\element ->
-                case element of
-                    Empty _ -> False
-                    _ -> True
-                )
-                elements
+        filteredElements =
+            filterEmptyElements elements
     in
-    case filterElements of
+    case filteredElements of
         [] -> mempty
-        _ -> [hsx|<div class="flex flex-col gap-y-5 md:gap-y-6">{forEach filterElements (\element -> element)}</div>|]
+        _ -> [hsx|<div class="flex flex-col gap-y-5 md:gap-y-6">{forEach filteredElements (\element -> element)}</div>|]
 
 
 
@@ -26,3 +21,13 @@ wrapContainerVerticalSpacingTiny element =
     case element of
         Empty _ -> [hsx||]
         _ -> [hsx|<div class="flex flex-col gap-y-2">{element}</div>|]
+
+
+filterEmptyElements :: [Html5.Html] -> [Html5.Html]
+filterEmptyElements elements =
+    filter (\element ->
+                case element of
+                    Empty _ -> False
+                    _ -> True
+                )
+                elements
