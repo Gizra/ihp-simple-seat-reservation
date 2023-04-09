d2eced8cfe28f0e39878b92aa373386c92565b08
Amitai Burstein <amitai@gizra.com>
2023-04-09 15:27:35 +0300

    Fix button

--- Web/Element/Button.hs
+++ Web/Element/Button.hs
@@ -8,7 +8,9 @@ import IHP.RouterSupport
 buildButton :: (IHP.RouterSupport.HasPath path) => Text -> path -> Html
 buildButton text action =
         [hsx|
-            <a href={pathTo $ action } class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:outline-none">{text}</a>
+            <div>
+                <a href={pathTo $ action } class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:outline-none">{text}</a>
+            </div>
         |]
 
 
