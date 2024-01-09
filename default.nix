bc01cc7816469d0e4316b67e678d1537133e8009
Amitai Burstein <amitai@gizra.com>
2024-01-09 13:32:48 +0200

    Migrate to 1.2 (#14)

--- default.nix
+++ default.nix
@@ -1,28 +1,9 @@
-let
-    ihp = builtins.fetchGit {
-        url = "https://github.com/digitallyinduced/ihp.git";
-        ref = "refs/tags/v1.0.1";
-        # If changing to a specific `rev` Execute:
-        # nix-shell --run 'make build/ihp-lib's
-        # rev = "05f1eafbe897dfdbf8a77e5bd0673b1857f4e8c2";
-    };
-    haskellEnv = import "${ihp}/NixSupport/default.nix" {
-        ihp = ihp;
-        haskellDeps = p: with p; [
-            cabal-install
-            base
-            wai
-            text
-            hlint
-            p.ihp
-        ];
-        otherDeps = p: with p; [
-            # Native dependencies, e.g. imagemagick
-            nodejs
-            # For testing concurrent requests.
-            parallel
-        ];
-        projectPath = ./.;
-    };
-in
-    haskellEnv
+# For backwards compatibility using flake.nix
+(import
+    (
+        fetchTarball {
+            url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
+            sha256 = "sha256:1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2";
+        }
+    )
+{ src = ./.; }).defaultNix
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+    inputs = {
+        # Here you can adjust the IHP version of your project
+        # You can find new releases at https://github.com/digitallyinduced/ihp/releases
+        ihp.url = "github:digitallyinduced/ihp/v1.2";
+        nixpkgs.follows = "ihp/nixpkgs";
+        flake-parts.follows = "ihp/flake-parts";
+        devenv.follows = "ihp/devenv";
+        systems.follows = "ihp/systems";
+    };
+
+    outputs = inputs@{ ihp, flake-parts, systems, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+
+            systems = import systems;
+            imports = [ ihp.flakeModules.default ];
+
+            perSystem = { pkgs, ... }: {
+                ihp = {
+                    enable = true;
+                    projectPath = ./.;
+                    packages = with pkgs; [
+                        # Native dependencies, e.g. imagemagick
+                        nodejs
+                        # For testing concurrent requests.
+                        parallel
+                    ];
+                    haskellPackages = p: with p; [
+                        # Haskell dependencies go here
+                        p.ihp
+                        cabal-install
+                        base
+                        wai
+                        text
+                        hlint
+                    ];
+                };
+            };
+
+        };
+}
